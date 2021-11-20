96beb7d04bf34898a8084982603812b9f095e396
MaximZoz <max591018@gmail.com>
2021-11-20 15:59:00 +0200

    first programm

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            string answer = $"Name: {name}, Age: {age}";
+
+            Console.WriteLine(answer);
+
+            int? a = null;
+            a?.ToString();
+
+            Console.WriteLine(a);
+
+
+
+
+        }
+    }
+}
