1cccb517d8bb19e3d83036175909190decf2f284
MaximZoz <max591018@gmail.com>
2021-11-21 15:24:19 +0200

    вычисление значения функции

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            const double E = 2.781281;
-            Console.WriteLine("{0: 0.00}", E);/* Console.WriteLine(Math.Round(Math.E, 2)); */
+            Console.WriteLine("Please, type your name ");
+            string user = Console.ReadLine();
+            Console.WriteLine($"Hello {user}. Today you have to computation some formula. Smile and press OK to start!");
+            Console.ReadLine();
+            Console.Write("Figure out value of function y=7x^2+3x+6. Please input value of x: ");
+            double valuex = Convert.ToDouble(Console.ReadLine());
+            double valuey = 7 * valuex * valuex + 3 * valuex + 6;
+            Console.WriteLine($"Easy {user}! Value of y = {valuey} . Press OK to pass on ");
+            Console.ReadLine();
         }
     }
 }
