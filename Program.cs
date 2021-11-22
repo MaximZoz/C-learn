69e44db26dcffab8ce3f212f0a9ae87016666e9a
MaximZoz <max591018@gmail.com>
2021-11-22 09:14:23 +0200

    посчитать периметр квадрата

--- Program.cs
+++ Program.cs
@@ -6,15 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please, type your name ");
-            string user = Console.ReadLine();
-            Console.WriteLine($"Hello {user}. Today you have to computation some formula. Smile and press OK to start!");
-            Console.ReadLine();
-            Console.Write("Figure out value of function y=7x^2+3x+6. Please input value of x: ");
-            double valuex = Convert.ToDouble(Console.ReadLine());
-            double valuey = 7 * valuex * valuex + 3 * valuex + 6;
-            Console.WriteLine($"Easy {user}! Value of y = {valuey} . Press OK to pass on ");
-            Console.ReadLine();
+            Console.WriteLine("Введите длину стороны квадрата:  ");
+            float s = Convert.ToSingle(Console.ReadLine());
+
+            if (s > 0)
+            {
+                float p = s * 4;
+                Console.Write($"\nПериметр квадрата  = {p}");
+            }
+            else
+                Console.Write("Введено не верное значение.");
         }
     }
 }
