ea6f4701d0f384e465df327371da22869609ff7e
MaximZoz <max591018@gmail.com>
2021-11-24 19:49:07 +0200

    число 1 или 2 через switch case

--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ namespace ConsoleApp1
             Console.WriteLine("Введите число");
             int num = Convert.ToInt32(Console.ReadLine());
             string str = String.Empty;
-            if (num == 1)
+            switch (num)
             {
-                str = "один";
-            }
-            else if (num == 2)
-            {
-                str = "два";
-            }
-            else
-            {
-                str = "не один и не два ";
+                case 0:
+                    str = "ноль";
+                    break;
+                case 1:
+                    str = "один";
+                    break;
+                default:
+                    str = "не ноль и не один";
+                    break;
             }
             Console.WriteLine($"вы ввели: {str}");
         }
