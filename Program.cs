71bda8c02e7fe048b4da9ba0bb3a6b526788b5ce
MaximZoz <max591018@gmail.com>
2021-11-24 20:13:23 +0200

    тернарный оператор

--- Program.cs
+++ Program.cs
@@ -6,22 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число");
-            int num = Convert.ToInt32(Console.ReadLine());
-            string str = String.Empty;
-            switch (num)
-            {
-                case 0:
-                    str = "ноль";
-                    break;
-                case 1:
-                    str = "один";
-                    break;
-                default:
-                    str = "не ноль и не один";
-                    break;
-            }
-            Console.WriteLine($"вы ввели: {str}");
+            string answer = null;
+            string res = answer == null ? ("answer null") : ("answer не null");
         }
     }
 }
