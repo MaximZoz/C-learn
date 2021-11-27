4a349ac12af9c42542ca656be8a0d3df26af532e
MaximZoz <max591018@gmail.com>
2021-11-27 14:20:58 +0200

    домашняя работа 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace HomeWork
+﻿namespace HomeWork
 {
     struct employees
     {
@@ -9,13 +7,23 @@ namespace HomeWork
         public int salaries;
     }
 
+    enum SortName
+    {
+        Alphabet = 1,
+        Salary = 2,
+        SalaryMoreAverage = 3,
+        SalaryLessAverage = 4
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             int quantity;
-            Console.WriteLine("Введите количество записей: ");
-            quantity = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Введите количество сотрудников: ");
+            quantity = SetValueForValidator();
+
             employees[] surnames = new employees[quantity];
             employees[] salaries = new employees[quantity];
 
@@ -27,17 +35,24 @@ namespace HomeWork
                 }
                 else if (i != quantity - 1)
                 {
-                    Console.WriteLine($"Введите фамилию следующего сотрудника: ");
+                    Console
+                        .WriteLine($"Введите фамилию следующего сотрудника: ");
                 }
                 else
                 {
-                    Console.WriteLine($"Введите фамилию последнего сотрудника: ");
+                    Console
+                        .WriteLine($"Введите фамилию последнего сотрудника: ");
                 }
                 surnames[i].surname = Convert.ToString(Console.ReadLine());
-                Console
-                    .WriteLine($"Введите зарплату сотрудника {surnames[i].surname}: ");
-                salaries[i].salaries = Convert.ToInt32(Console.ReadLine());
+
+                string surname = surnames[i].surname;
+                Console.WriteLine($"Введите зарплату сотрудника {surname}: ");
+
+                salaries[i].salaries = SetValueForValidator();
             }
+
+            Console.WriteLine("1,2,3,4");
+            string str = CallSortingMethod();
 
             Console.WriteLine("N     |      Фамилия         | Зарплата |");
             Console.WriteLine("----------------------------------------");
@@ -52,6 +67,60 @@ namespace HomeWork
                 Console.WriteLine("----------------------------------------");
             }
             Console.Read();
+        }
+
+        static string CallSortingMethod()
+        {
+            byte num;
+            try
+            {
+                num = Convert.ToByte(Console.ReadLine());
+            }
+            catch
+            {
+                num = 5;
+            }
+            string str;
+            switch (num)
+            {
+                case (byte) SortName.Alphabet:
+                    str = "Алфавит";
+                    break;
+                case (byte) SortName.Salary:
+                    str = "Зарплата";
+                    break;
+                case (byte) SortName.SalaryMoreAverage:
+                    str = "Зарплата выше среднего";
+                    break;
+                case (byte) SortName.SalaryLessAverage:
+                    str = "Зарплата ниже среднего";
+                    break;
+                default:
+                    str = "";
+                    break;
+            }
+            if (str == "")
+            {
+                Console.WriteLine("неправильно, попробуйте снова");
+                CallSortingMethod();
+            }
+            return str;
+        }
+
+        static int SetValueForValidator()
+        {
+            int val;
+            try
+            {
+                val = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Неправильное значение, попробуйте снова!");
+                SetValueForValidator();
+                val = 1;
+            }
+            return val;
         }
     }
 }
