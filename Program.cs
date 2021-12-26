3f1238adcaf4bde8347db8eae93e5067940d1004
MaximZoz <max591018@gmail.com>
2021-12-26 20:22:19 +0200

    крестики-нолики

--- Program.cs
+++ Program.cs
@@ -1,13 +1,48 @@
-﻿using C_learn;
+﻿using System;
+using System.Text;
 
 namespace ConsoleApp1
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static TicTacToeGame G = new TicTacToeGame();
+
+        private static void Main()
         {
-            var game = new GuessNumberGame(20, 5, GuessingPlayers.Machine);
-            game.Start();
+            Console.WriteLine(GetPrintableState());
+            while (G.GetWinner() == Winner.GameIsUnfinished)
+            {
+                var index = int.Parse(Console.ReadLine() ?? string.Empty);
+                G.MakeMove(index);
+                Console.WriteLine();
+                Console.WriteLine(GetPrintableState());
+            }
+
+            Console.WriteLine($"Result: {G.GetWinner()}");
+        }
+
+        private static string GetPrintableState()
+        {
+            var sb = new StringBuilder();
+            for (var i = 1; i <= 7; i += 3)
+            {
+                sb.AppendLine("     |     |     |")
+                    .AppendLine($"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  |")
+                    .AppendLine("_____|_____|_____|");
+            }
+
+            return sb.ToString();
+        }
+
+        private static string GetPrintableChar(int index)
+        {
+            var state = G.GetState(index);
+            if (state == State.Unset)
+            {
+                return index.ToString();
+            }
+
+            return state == State.Cross ? "X" : "O";
         }
     }
 }
---
+++ TicTacToeGame/TicTaeToeGame.cs
@@ -0,0 +1,69 @@
+﻿namespace ConsoleApp1
+{
+    public enum Winner
+    {
+        Crosses,
+        Zeroes,
+        Draw,
+        GameIsUnfinished
+    }
+
+    public enum State
+    {
+        Cross,
+        Zero,
+        Unset
+    }
+
+    public class TicTacToeGame
+    {
+        private readonly State[] _board = new State[9];
+
+        public int MovesCounter { get; private set; }
+
+        public TicTacToeGame()
+        {
+            for (var i = 0; i < _board.Length; i++)
+            {
+                _board[i] = State.Unset;
+            }
+        }
+
+        public void MakeMove(int index)
+        {
+            _board[index - 1] = MovesCounter % 2 == 0 ? State.Cross : State.Zero;
+            MovesCounter++;
+        }
+
+        public State GetState(int index)
+        {
+            return _board[index - 1];
+        }
+
+        public Winner GetWinner()
+        {
+            return GetWinner(1, 4, 7, 2, 5, 8, 3, 6, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 5, 9, 3, 5, 7);
+        }
+
+        private Winner GetWinner(params int[] indexes)
+        {
+            for (var i = 0; i < indexes.Length; i += 3)
+            {
+                var same = AreSame(indexes[i], indexes[i + 1], indexes[i + 2]);
+                if (!same) continue;
+                var state = GetState(indexes[i]);
+                if (state != State.Unset)
+                {
+                    return state == State.Cross ? Winner.Crosses : Winner.Zeroes;
+                }
+            }
+
+            return MovesCounter < 9 ? Winner.GameIsUnfinished : Winner.Draw;
+        }
+
+        private bool AreSame(int a, int b, int c)
+        {
+            return GetState(a) == GetState(b) && GetState(a) == GetState(c);
+        }
+    }
+}
